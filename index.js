09fbd726ebf23a69244a08fcc0e12d36c06568da
polyzer <realpolyzer@yandex.ru>
2020-03-26 12:22:46 +0500

    update

--- index.js
+++ index.js
@@ -25,6 +25,25 @@ import { version } from '@tensorflow/tfjs-backend-wasm/dist/version';
 
 import { TRIANGULATION } from './triangulation';
 
+const visibleHeightAtZDepth = (depth, camera) => {
+  // compensate for cameras not positioned at z=0
+  const cameraOffset = camera.position.z;
+  if (depth < cameraOffset) depth -= cameraOffset;
+  else depth += cameraOffset;
+
+  // vertical fov in radians
+  const vFOV = camera.fov * Math.PI / 180;
+
+  // Math.abs to ensure the result is always positive
+  return 2 * Math.tan(vFOV / 2) * Math.abs(depth);
+};
+
+const visibleWidthAtZDepth = (depth, camera) => {
+  const height = visibleHeightAtZDepth(depth, camera);
+  return height * camera.aspect;
+};
+
+
 tfjsWasm.setWasmPath(
   `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${
   version}/dist/tfjs-backend-wasm.wasm`);
@@ -51,7 +70,7 @@ function drawPath(ctx, points, closePath) {
 const VIDEO_HEIGHT = window.innerHeight;
 const VIDEO_WIDTH = window.innerWidth;
 let model, ctx, videoWidth, videoHeight, video, canvas,
-  scatterGLHasInitialized = false, scatterGL;
+  scatterGLHasInitialized = false, scatterGL, flattenedPointsData = [], facePrediction = null;
 
 class Program {
   constructor(width, height) {
@@ -61,6 +80,7 @@ class Program {
       width: width,
       height: height
     };
+
     this.Container = document.createElement("div");
     this.Container.id = "container3d";
     this.Container.style.position = "absolute";
@@ -69,11 +89,89 @@ class Program {
     document.body.appendChild(this.Container);
     this.Scene = new THREE.Scene();
     this.Camera = new THREE.PerspectiveCamera(45, this.SCREEN_WIDTH / this.SCREEN_HEIGHT, 0.1, 10000);
-    this.Camera.position.set(0, 0, 100);
+    this.Camera.position.set(this.SCREEN_WIDTH / 2, this.SCREEN_HEIGHT / 2, this.SCREEN_HEIGHT / visibleHeightAtZDepth(1, this.Camera) * 1);
     this.Scene.add(this.Camera);
     this.Video = document.querySelector("#video");
     this.Object = new THREE.Object3D();
+    this.HelmetScene = null;
+    this.HelmetHead = null;
+    this.HelmetHeadObject = null;
     // this.Scene.add(new THREE.Mesh(new THREE.SphereBufferGeometry(10, 10, 10), new THREE.MeshBasicMaterial()));
+
+
+
+    let gltfloader = new THREE.GLTFLoader();
+    gltfloader.load(
+      // resource URL
+      'Mask.glb',
+      // called when the resource is loaded
+      function (gltf) {
+        gltf.scene.traverse(function (child) {
+
+          if (child.name === "Human_02") {
+            this.HelmetHead = child;
+            if (!this.HelmetHead.geometry.boundingBox) {
+              this.HelmetHead.geometry.computeBoundingBox();
+              this.HelmetHead.material.metalness = 0;
+            }
+          }
+          if (child.name === "entity_2") {
+            this.HelmetHeadObject = child;
+            this.HelmetHeadObject.position.set(100, 100, 0);
+            for (let chd of this.HelmetHeadObject.children) {
+              chd.material.metalness = 0;
+            }
+          }
+          console.log(child);
+
+        }.bind(this));
+        this.HelmetScene = gltf.scene;
+        this.Scene.add(this.HelmetScene);
+
+      }.bind(this),
+      // called while loading is progressing
+      function (xhr) {
+
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+      },
+      // called when loading has errors
+      function (error) {
+
+        console.log('An error happened');
+        console.log(error);
+
+      }
+    );
+
+    // let fbxloader = new THREE.FBXLoader();
+    // fbxloader.load('Mask.fbx', function (object) {
+
+    //   //mixer = new THREE.AnimationMixer(object);
+
+    //   //var action = mixer.clipAction(object.animations[0]);
+    //   //action.play();
+
+    //   object.traverse(function (child) {
+
+    //     if (child.isMesh) {
+    //       if (child.name === "Human_02001") {
+    //         this.HelmetHead = child;
+    //         if (!this.HelmetHead.geometry.boundingBox) {
+    //           this.HelmetHead.geometry.computeBoundingBox();
+    //           this.HelmetHead.position.set(100, 100, 0);
+    //         }
+    //       }
+    //       child.castShadow = true;
+    //       child.receiveShadow = true;
+
+    //     }
+
+    //   });
+
+    //   this.Scene.add(object);
+
+    // });
 
     let backPlaneMat = new THREE.PointsMaterial({ color: 0xFF0000 });
     let backPlaneGeom = new THREE.BufferGeometry();
@@ -95,6 +193,9 @@ class Program {
     //this.Video.autoplay = 1;
     // this.Video.width = 224;
     // this.Video.height = 224;
+
+    let light = new THREE.AmbientLight(0xffffff); // soft white light
+    this.Scene.add(light);
 
     this.ctx = document.querySelector("#output").getContext("2d");
     this.ctx.canvas.width = this.meshes_parameters.width;
@@ -129,12 +230,39 @@ class Program {
     this.Renderer.autoClearColor = false;
     this.Container.appendChild(this.Renderer.domElement);
     this.render = this.render.bind(this);
+
+    this.mesh3dMat = new THREE.PointsMaterial({ color: 0xFF0000, size: 3 });
+    this.mesh3dGeom = new THREE.BufferGeometry();
+    this.mesh3dGeom.addAttribute('position', new THREE.Float32BufferAttribute([], 3));
+    this.FaceMesh3D = new THREE.Points(this.mesh3dGeom, this.mesh3dMat);
+    this.Scene.add(this.FaceMesh3D);
+  }
+
+  renderFacePoints(coords) {
+    let flat = coords.flat();
+    this.mesh3dGeom.addAttribute('position', new THREE.Float32BufferAttribute(flat, 3));
+    this.mesh3dGeom.attributes.position.needsUpdate = true;
+  }
+  renderHelmetHead(faceprediction) {
+    this.HelmetHead.geometry.computeBoundingBox();
+    let width = faceprediction.boundingBox.bottomRight[0][0] - faceprediction.boundingBox.topLeft[0][0];
+    let height = faceprediction.boundingBox.bottomRight[0][1] - faceprediction.boundingBox.topLeft[0][1];
+
+    let head_width = this.HelmetHead.geometry.boundingBox.max.x - this.HelmetHead.geometry.boundingBox.min.x;
+    let head_height = this.HelmetHead.geometry.boundingBox.max.y - this.HelmetHead.geometry.boundingBox.min.y;
+    let val = width / head_width / 9;
+    this.HelmetHeadObject.scale.set(val, val, val);
+
   }
   render(coords) {
     //this.imageCanvasTexture.needsUpdate = true;
     this.CanvasTexture.needsUpdate = true;
     // this.imageMesh.position.x = coords.x;
     // this.imageMesh.position.y = coords.y;
+    this.renderFacePoints(flattenedPointsData);
+    if (facePrediction && this.HelmetHead) {
+      this.renderHelmetHead(facePrediction);
+    }
     this.Renderer.render(this.Scene, this.Camera);
     //requestAnimationFrame(this.render);
   }
@@ -207,6 +335,7 @@ async function renderPrediction() {
   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
   if (predictions.length > 0) {
+    facePrediction = predictions[0];
     predictions.forEach(prediction => {
       const keypoints = prediction.scaledMesh;
 
@@ -234,10 +363,11 @@ async function renderPrediction() {
     if (renderPointcloud && state.renderPointcloud && scatterGL != null) {
       const pointsData = predictions.map(prediction => {
         let scaledMesh = prediction.scaledMesh;
-        return scaledMesh.map(point => ([-point[0], -point[1], -point[2]]));
+        return scaledMesh.map(point => ([point[0], -point[1] + VIDEO_HEIGHT, point[2]]));
       });
+      //const pointsData = predictions[0].scaledMesh;
 
-      let flattenedPointsData = [];
+      flattenedPointsData = [];
       for (let i = 0; i < pointsData.length; i++) {
         flattenedPointsData = flattenedPointsData.concat(pointsData[i]);
       }
@@ -251,6 +381,8 @@ async function renderPrediction() {
       scatterGLHasInitialized = true;
     }
 
+  } else {
+    facePrediction = null;
   }
   prog.render();
   stats.end();
@@ -283,8 +415,8 @@ async function main() {
   canvasContainer.style = `width: ${VIDEO_WIDTH}px; height: ${VIDEO_HEIGHT}px`;
 
   ctx = canvas.getContext('2d');
-  // ctx.translate(canvas.width, 0);
-  // ctx.scale(-1, 1);
+  ctx.translate(canvas.width, 0);
+  ctx.scale(-1, 1);
   ctx.fillStyle = '#32EEDB';
   ctx.strokeStyle = '#32EEDB';
   ctx.lineWidth = 0.5;
